aa96d14297d9a04f37b233322d1d94e75019bd2b
Dario <80409852+dariomartinovski@users.noreply.github.com>
2023-12-10 19:47:23 +0100

    Deleted commented code from CityRepo and WineryRepo. Refactored code in InMemoryDataHolder

--- src/main/java/wineverse/com/mk/Wineverse/Repository/CityRepository.java
+++ src/main/java/wineverse/com/mk/Wineverse/Repository/CityRepository.java
@@ -8,22 +8,6 @@ import java.util.List;
 
 @Repository
 public class CityRepository {
-//    List<String> all_cities = Arrays.asList(
-//            "Берово", "Битола", "Богданци", "Валандово", "Велес", "Виница", "Гевгелија",
-//            "Гостивар", "Дебар", "Делчево", "Демир Капија", "Демир Хисар", "Кавадарци",
-//            "Кичево", "Кочани", "Кратово", "Крива Паланка", "Крушево", "Куманово",
-//            "Македонски Брод", "Македонска Каменица", "Неготино", "Охрид", "Пехчево",
-//            "Прилеп", "Пробиштип", "Радовиш", "Ресен", "Свети Николе", "Скопје",
-//            "Струга", "Струмица", "Тетaово", "Штип");
-//    @Getter
-//    List<City> cities = new ArrayList<>();
-//
-//    public CityRepository() {
-//        for(String city : all_cities){
-//            cities.add(new City(city));
-//        }
-//    }
-
     public City findCity(String name) {
        return InMemoryDataHolder.cities.stream()
                .filter(city->city.getName().equals(name))
--- src/main/java/wineverse/com/mk/Wineverse/Repository/WineryRepository.java
+++ src/main/java/wineverse/com/mk/Wineverse/Repository/WineryRepository.java
@@ -8,27 +8,6 @@ import java.util.List;
 
 @Repository
 public class WineryRepository {
-//    private final CityRepository cityRepository;
-//    private final List<Winery>  wineryRepository = new ArrayList<>();
-//
-//    @Autowired
-//    public WineryRepository(CityRepository cityRepository) {
-//        this.cityRepository = cityRepository;
-//        initializeWineries();
-//    }
-//    private void initializeWineries() {
-//        Winery w1 = new Winery("Popova Kula","Wine Boulevard No.1 Demir Kapija","Понеделник-Петок 10:00-22:00", cityRepository.find_city("Демир Капија"),"http://www.popovakula.com.mk/", "gglmapsid",	"076 432 630",	"+389 76 432 630", (float) 41.22, (float) 22.2);
-//        cityRepository.find_city("Демир Капија").add_winery_in_city(w1);
-//        Winery w2 = new Winery("Wine Tours Macedonia",		"8-mi Mart 18, Skopje", 	"Понеделник-Петок 10:00-22:00",cityRepository.find_city("Скопје"),"https://www.winetours.mk/","gglsmaps21w", "070 958 307",	"+389 70 958 307", (float) 42.009, (float) 21.424);
-//        cityRepository.find_city("Скопје").add_winery_in_city(w2);
-//        Winery w3 = new Winery("Popova Sapka","Wine Boulevard No.1 Demir Kapija","Понеделник-Петок 10:00-22:00", cityRepository.find_city("Демир Капија"),"http://www.popovakula.com.mk/", "gglmapsid",	"076 432 630",	"+389 76 432 630", (float) 41.22, (float) 22.2);
-//        cityRepository.find_city("Демир Капија").add_winery_in_city(w3);
-//
-//        wineryRepository.add(w1);
-//        wineryRepository.add(w2);
-//        wineryRepository.add(w3);
-//    }
-
     public List<Winery> getAllWineries() {
         return InMemoryDataHolder.wineryList;
     }

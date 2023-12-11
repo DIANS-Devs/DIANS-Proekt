d6b97774a098841b1962732def64c1c81c116f74
Dario <80409852+dariomartinovski@users.noreply.github.com>
2023-12-11 21:40:53 +0100

    Refactored the models a bit, deleted some, changed others

---
+++ src/main/java/wineverse/com/mk/Wineverse/Model/Type.java
@@ -0,0 +1,25 @@
+package wineverse.com.mk.Wineverse.Model.Modelv1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String type;
+
+    public Type() {
+    }
+
+    public Type(String type) {
+        //TODO For testing uncomment this
+//        Id = (long) (Math.random() * 1000);
+        this.type = type;
+    }
+}

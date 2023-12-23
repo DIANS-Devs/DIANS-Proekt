7b89ce688f53d969c891092d665b923cf1d75e3a
MCA\dip <dip@mca.dev>
2023-12-23 18:25:34 +0100

    changed the cookie

--- Homework 3/src/main/resources/static/js/favoriteButton.js
+++ Homework 3/src/main/resources/static/js/favoriteButton.js
@@ -1,37 +1,34 @@
-function getCookie(name) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.indexOf(name + '=') === 0) {
-            return cookie.substring((name + '=').length, cookie.length);
+function getFavorites() {
+    const cookies = document.cookie.split(';');
+    let favoritesCookie = '';
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf('favorites=') === 0) {
+            favoritesCookie = cookie.substring(('favorites=').length, cookie.length);
         }
     }
-    return null;
-}
-function setCookie(name, value, days) {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = '; expires=' + date.toUTCString();
-    }
-    document.cookie = name + '=' + value + expires + '; path=/';
-}
-function getFavoritesFromCookie() {
-    var favoritesCookie = getCookie('favorites');
-    return favoritesCookie ? JSON.parse(favoritesCookie) : [];
-}
-function setFavoritesCookie(favorites) {
-    setCookie('favorites', JSON.stringify(favorites), 365);
+
+    return favoritesCookie;
 }
 
-function addToFavorites(element) {
-    //TODO modify here later...
-    let wineryId = element.getAttribute('data-winery-id');
-    let child = element.querySelector('i');
-    if (wineryId.trim() !== '') {
-        var favorites = getFavoritesFromCookie();
-        var index = favorites.indexOf(wineryId);
+function setFavorites(values) {
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 365);
+    document.cookie = `favorites=${values}; path=/; expires=${expirationDate.toUTCString()}`;
+}
+
+function addOrRemoveFavorite(element) {
+    const wineryId = element.getAttribute('data-winery-id');
+    const child = element.querySelector('i');
+    let favorites = getFavorites().split(',');
+    if (favorites[0] === ''){
+        favorites.pop();
+        favorites.push(wineryId);
+        child.classList.remove("far");
+        child.classList.add("fas");
+    }
+    else {
+        const index = favorites.indexOf(wineryId);
         if (index === -1) {
             favorites.push(wineryId);
             child.classList.remove("far");
@@ -41,32 +38,22 @@ function addToFavorites(element) {
             child.classList.remove("fas");
             child.classList.add("far");
         }
-        setFavoritesCookie(favorites);
-
-        favoriteButton(favorites);
     }
+    let favString = favorites.join(',');
+    setFavorites(favString);
 }
+
 function checkFavorite(element){
-    var wineryId = element.getAttribute('data-winery-id');
+    let wineryId = element.getAttribute('data-winery-id');
     let child = element.querySelector('i');
     if (wineryId.trim() !== '') {
-        let favorites = getFavoritesFromCookie();
+        let favorites = getFavorites();
         let index = favorites.indexOf(wineryId);
         if (index > -1) {
             child.classList.remove("far");
             child.classList.add("fas");
         }
     }
-}
-
-function favoriteButton(favoritesList) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/favorites-list", true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify(favoritesList));
-}
-function refreshPage() {
-    location.reload(true);
 }
 
 window.onload = function() {

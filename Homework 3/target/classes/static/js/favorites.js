87dd83898253d80b3e4cfac7ee26921fa3f2fe05
Dario <master15820@gmail.com>
2024-01-28 14:47:38 +0100

    New target folder

--- Homework 3/target/classes/static/js/favorites.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let favorites = document.getElementsByClassName('favorite_container');
-    Array.from(favorites).forEach(function(favorite, index) {
-        favorite.addEventListener('click', function() {
-            let form = document.getElementById("favoriteForm" + index);
-            form.submit();
-        });
-    });
-});

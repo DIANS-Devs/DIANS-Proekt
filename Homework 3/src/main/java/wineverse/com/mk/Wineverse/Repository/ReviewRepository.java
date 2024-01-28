adba42bca6f4de9deece80c19131d67b2344ad4a
MCA\dip <dip@mca.dev>
2024-01-28 16:20:53 +0100

    reverted changes 3

--- Homework 3/src/main/java/wineverse/com/mk/Wineverse/Repository/ReviewRepository.java
+++ Homework 3/src/main/java/wineverse/com/mk/Wineverse/Repository/ReviewRepository.java
@@ -3,5 +3,10 @@ package wineverse.com.mk.Wineverse.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import wineverse.com.mk.Wineverse.Model.Review;
+
+import java.util.List;
+
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> { }
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllById(Long Id);
+}
--- Homework 3/src/main/java/wineverse/com/mk/Wineverse/Repository/WineryRepository.java
+++ Homework 3/src/main/java/wineverse/com/mk/Wineverse/Repository/WineryRepository.java
@@ -2,7 +2,14 @@ package wineverse.com.mk.Wineverse.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import wineverse.com.mk.Wineverse.Model.City;
 import wineverse.com.mk.Wineverse.Model.Winery;
 
+import java.util.List;
+
 @Repository
-public interface WineryRepository extends JpaRepository<Winery, Long> { }
+public interface WineryRepository extends JpaRepository<Winery, Long> {
+    List<Winery> findByCity(City city);
+    List<Winery> findByNameContaining(String name);
+    List<Winery> findByRatingGreaterThanEqual(Float rating);
+}
